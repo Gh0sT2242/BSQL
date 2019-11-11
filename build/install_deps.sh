c2185d94a3a3fc082b735dc276c260b4cce7b7fc
Gh0sT2242 <34068333+Gh0sT2242@users.noreply.github.com>
2019-11-10 21:07:12 -0600

    Fix BYOND download in install_deps.sh

--- build/install_deps.sh
+++ build/install_deps.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-if [ -d "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin" ];
+if [ -d "$HOME/BYOND/byond/bin" ];
 then
   echo "Using cached BYOND directory."
 else
   echo "Setting up BYOND."
-  mkdir -p "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
-  cd "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
-  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+  mkdir -p "$HOME/BYOND}"
+  cd "$HOME/BYOND}"
+  curl "http://www.byond.com/download/build/512/512.1488_byond_linux.zip" -o byond.zip
   unzip byond.zip
   cd byond
   make here
